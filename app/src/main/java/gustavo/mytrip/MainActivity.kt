33a5a81edd567f6e848b2f45990c414fb4801cd6
gusttavosouza <gustavorobertotb@outlook.com>
2022-09-20 18:26:49 -0300

    feat: add logic

--- app/src/main/java/gustavo/mytrip/MainActivity.kt
+++ app/src/main/java/gustavo/mytrip/MainActivity.kt
@@ -3,7 +3,9 @@ package gustavo.mytrip
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun handleCalculate() {
+        if(isValid()){
 
+            try {
+                val distance = editDistance.text.toString().toFloat();
+                val preco = editPreco.text.toString().toFloat();
+                val autonomia = editAutonomia.text.toString().toFloat();
+                val result = ((distance * preco) / autonomia)
+                textResult.setText("Total: RS $result")
+            }catch (nfe: NumberFormatException){
+                Toast.makeText(this,"Por favor, informe valores válidos!", Toast.LENGTH_LONG).show()
+            }
+        }else {
+            Toast.makeText(this,"Por favor, informe valores válidos!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return editDistance.text.toString() != ""
+                && editPreco.text.toString() != ""
+                && editAutonomia.text.toString() != ""
+                && editAutonomia.text.toString() != "0"
     }
 
 }
